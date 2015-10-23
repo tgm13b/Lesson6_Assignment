1b4d63069ab8872d13f5001695b7b471d55f443a
tgm13b <tgm13b@my.fsu.edu>
2015-10-23 22:27:31 +0000

    Lesson 6 Assignment First Commit

---
+++ Lesson6_Assignment/zoom.js
@@ -0,0 +1,32 @@
+/*  Program Name:  Photo Gallery Application
+    Author: Tyler Martin
+    Date:   10/23/15
+    Filename: zoom.js
+ */
+
+"use strict"; // interpret document contents in JavaScript strict mode
+
+/* global variables */
+var photoOrderArray = window.opener.photoOrder;
+var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
+
+/* populate img element and create event listener */
+function pageSetup() {
+   document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+  createEventListener();
+}
+/* close window */
+function closeWin() {
+  window.close();
+}
+/* create event listener for close button */
+function createEventListener() {
+  var closeWindowDiv = document.getElementsByTagName("p")[0];
+  if (closeWindowDiv.addEventListener) {
+    closeWindowDiv.addEventListener("click", closeWin, false);
+  } else if (closeWindowDiv.attachEvent) {
+    closeWindowDiv.attachEvent("onclick", closeWin);
+  }
+}
+/* add img src value and create event listener when page finishes loading */
+window.onload = pageSetup;
